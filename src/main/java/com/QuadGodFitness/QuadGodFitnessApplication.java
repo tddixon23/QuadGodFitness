ca4dbc59dc84094fc9ea40bf3b4d8403b688bf36
tddixon0 <119270651+tddixon0@users.noreply.github.com>
2024-06-19 00:48:56 -0400

    move structure

--- src/main/java/com/QuadGodFitness/QuadGodFitnessApplication.java
+++ src/main/java/com/QuadGodFitness/QuadGodFitnessApplication.java
@@ -1,4 +1,4 @@
-package com.QuadGodFitness.main;
+package com.QuadGodFitness;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
