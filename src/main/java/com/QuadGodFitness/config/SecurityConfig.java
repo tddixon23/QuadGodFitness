a7b0fa330c1c0137d34e4f18f67eb0aaa50b5e47
tddixon0 <119270651+tddixon0@users.noreply.github.com>
2024-06-26 12:35:37 -0400

    fix deprication

--- src/main/java/com/QuadGodFitness/config/SecurityConfig.java
+++ src/main/java/com/QuadGodFitness/config/SecurityConfig.java
@@ -1,45 +1,30 @@
 package com.QuadGodFitness.config;
 
 
-import com.QuadGodFitness.filter.JwtFilter;
-import com.QuadGodFitness.service.CustomUserDetailsService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
-    @Autowired
-    private JwtFilter jwtFilter;
-
-    @Autowired
-    private CustomUserDetailsService customUserDetailsService;
-
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.csrf().disable()
-            .authorizeRequests(authorizeRequests ->
-                authorizeRequests
-                    .requestMatchers("/api/users/register", "/api/users/login").permitAll()
-                    .requestMatchers("/api/workouts/**").authenticated()
-                    .anyRequest().permitAll()
+        http
+            .csrf(csrf -> csrf.disable())
+            .authorizeHttpRequests(auth -> auth
+                .requestMatchers("/api/**").authenticated()
             )
-            .sessionManagement(sessionManagement ->
-                sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-            );
-
-        http.addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
+            .httpBasic(withDefaults());
+        
         return http.build();
     }
 
